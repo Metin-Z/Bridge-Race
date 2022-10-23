f6fe02bd5389e6e3ee8cb9c9165ad4d424ef60b9
Metin-Z <metinzehiroglu34@gmail.com>
2022-10-23 16:09:17 +0300

    navmesh

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.AI;
+using System.Linq;
 public class AIController : MonoBehaviour
 {
     public static AIController instance;
     public Transform BaseSlot;
     public float speed;
-    Animator anim;
+    Animator _anim;
+    NavMeshAgent _navmesh;
     public List<Transform> BlockList;
     public int randomBlock;
-    public Vector3 target;
+    public GameObject target;
+
     public virtual void Awake()
     {
         if (!instance)
@@ -25,27 +29,48 @@ public class AIController : MonoBehaviour
     }
     private void Start()
     {
-        anim = GetComponent<Animator>();
-        //StartCoroutine(AIMovement());
+        _anim = GetComponent<Animator>();
+        _navmesh = GetComponent<NavMeshAgent>();
+        StartCoroutine(AIMovement());
     }
     private void Update()
     {
         if (!GameManager.instance.isGameRunning)
             return;
-        //  randomBlock = Random.Range(0, GameManager.instance.AISpawnedBlocks.Count);
-        //  target = GameManager.instance.AISpawnedBlocks[randomBlock].transform.position;
+
     }
+    public GameObject myBridge;
     public IEnumerator AIMovement()
     {
-        yield return new WaitForSeconds(2);
-        if (GameManager.instance.AISpawnedBlocks !=null)
-        {
-            
-            Sequence sequence1 = DOTween.Sequence();
 
-            sequence1.Join(transform.DOMove(target, 4.5f).SetLoops(6));
-  
+        while (!GameManager.instance.isGameRunning)
+            yield return new WaitForSeconds(.1f);
+
+        yield return new WaitForFixedUpdate();
+
+        while (GameManager.instance.isGameRunning)
+        {
+            if (!target)
+            {
+                target = myBridge.GetComponent<PlayerBridge1>().AIBricks1.Where(x => x.activeSelf).OrderBy(x => Vector3.Distance(x.transform.position, transform.position)).FirstOrDefault();
+            }
+            else if (target)
+            {
+                if (BlockList.Count() > Random.Range(10, 19))
+                {
+                    int count = BlockList.Count();
+                    while (Vector3.Distance(transform.position, myBridge.GetComponent<PlayerBridge1>().AIBricks1[0].transform.position) > 0.2f)
+                    {
+                        _navmesh.SetDestination(myBridge.GetComponent<PlayerBridge1>().AIBricks1[0].transform.position);
+                        yield return new WaitForSeconds(.1f);
+                    }
+                    yield return new WaitForSeconds(.15f * count);
+                }
+            }
         }
+
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -73,16 +98,16 @@ public class AIController : MonoBehaviour
     }
     public void EndGameWin()
     {
-        anim.SetBool("Run", false);
-        anim.SetBool("Dance", true);
+        _anim.SetBool("Run", false);
+        _anim.SetBool("Dance", true);
         Quaternion target = Quaternion.Euler(0, 180, 0);
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 200);
 
     }
     public void EndGameLose()
     {
-        anim.SetBool("Run", false);
-        anim.SetBool("Sad", true);
+        _anim.SetBool("Run", false);
+        _anim.SetBool("Sad", true);
         Quaternion target = Quaternion.Euler(0, 180, 0);
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 200);
 
